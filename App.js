11ae691fe061f6655a26f1d938e86022f3a2a4d3
nmi <nmi@rismasystems.com>
2024-02-29 18:40:48 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/homepage/index.html");
+});
+
+const PORT = 8080;
+app.listen(PORT, () => console.log("Server is running on port: ", PORT));
